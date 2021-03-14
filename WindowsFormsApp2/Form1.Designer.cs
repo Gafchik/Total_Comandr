dc75518e3b1afe755988892896352e1e89db044b
student <student@COMP-209>
2021-03-14 13:05:03 +0200

    Жесть

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.Color.White;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(464, 426);
+            this.treeView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(482, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 425);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 85);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 168);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,43 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        private void add_tree()
+        {
+            string[] dir = Environment.GetLogicalDrives();
+           
+            foreach (var item in dir.ToList<string>())
+            {
+                treeView1.Nodes.Add(item);
+                add_tree(item);
+            }
+        }
+        private void add_tree(string path)
+        {
+            try
+            {
+                string[] dir = Directory.GetDirectories(path);
+
+                foreach (var item in dir.ToList<string>())
+                {
+                    treeView1.Nodes.Add(item);
+                    add_tree(item);
+                }
+            }
+            catch (Exception) { }
+          
+        }
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            add_tree();
+           // Directory.GetLogicalDrives();
+            //Environment.GetLogicalDrives().ToList<string>().ForEach(i => textBox1.Text += i + "\r\n");
+          
         }
     }
 }
